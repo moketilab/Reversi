42a74c952d1173d2b1d0c13fd2b1b9e85bd707b5
moketilab <moketi@ta-ru.com>
2018-05-05 02:53:12 +0900

    Improve performance

--- PyServer/reversi.py
+++ PyServer/reversi.py
@@ -9,6 +9,32 @@ SECOND = 2
 def Enemy(p):
     return SECOND if (p == FIRST) else FIRST
 
+class Move(object):
+    """ Move for Reversi """
+    FIRST  = FIRST
+    SECOND = SECOND
+    def __init__(self, p, x, y, *args, **kwargs):
+        self.player = p
+        self.x = x
+        self.y = y
+        return super().__init__(*args, **kwargs)
+    def __str__(self):
+        if(self.player == Move.FIRST):
+            rv = "ABCDEFGH"[self.x]+"12345678"[self.y]
+        elif(self.player == Move.SECOND):
+            rv = "abcdefgh"[self.x]+"12345678"[self.y]
+        else:
+            rv = "ERROR"
+        return rv
+    def GetX(self):
+        return self.x
+    def GetY(self):
+        return self.y
+    def Me(self):
+        return self.player
+    def Enemy(self):
+        return Enemy(self.player)
+
 class Board(object):
     """ Board status for Reversi """
     NONE = 0
@@ -19,17 +45,20 @@ class Board(object):
         self._BoardInit();
         return super().__init__(*args, **kwargs)
     #### Public
-    def Move(self, move):
-        return self._Move(self.board, move.GetX(), move.GetY(), move.Me(), move.Enemy())
-    def MoveCheck(self, move):
-        return self._Move(self.board, move.GetX(), move.GetY(), move.Me(), move.Enemy(), True)
+    def Move(self, move:Move):
+        return self._Move(move.GetX(), move.GetY(), move.Me(), move.Enemy())
+    def MoveCheck(self, move:Move):
+        return self._Move(move.GetX(), move.GetY(), move.Me(), move.Enemy(), True)
     def GenMoves(self, player):
         """ retrun list of Move """
         moves = [Move(player,x,y) for x in range(8) for y in range(8)]
         return [x for x in moves if self.MoveCheck(x)]
     def CanMove(self, player):
-        ms = self.GenMoves(player)
-        return len(ms) > 0
+        for x in range(8):
+           for y in range(8):
+               if(self.MoveCheck(Move(player,x,y))):
+                   return True
+        return False
     def IsGameEnd(self):
         return not (self.CanMove(FIRST) or self.CanMove(SECOND))
     def Score(self):
@@ -61,76 +90,45 @@ class Board(object):
         self.board[4][3] = Board.FIRST
         self.board[3][3] = Board.SECOND
         self.board[4][4] = Board.SECOND
-    def _Move(self,b,x,y,p,e,c=False):
+    def _Move(self,x,y,p,e,c=False):
+        b = self.board
         rv = False
         if(b[y][x]!=Board.NONE):
             return rv
         if(c):
             for d in Board.DIR:
-                rv = rv or self._MoveLine(b,x,y,d[0],d[1],p,e,c)
+                rv = rv or self._MoveLine(x,y,d[0],d[1],p,e,c)
         else:
             for d in Board.DIR:
-                rv = self._MoveLine(b,x,y,d[0],d[1],p,e,c) or rv
+                rv = self._MoveLine(x,y,d[0],d[1],p,e,c) or rv
             if(rv):
                 b[y][x]=p
         return rv
-    def _MoveLine(self,b,x,y,dx,dy,p,e,c=False):
-        if(not c):
-            bs = copy.deepcopy(b)
-        else:
-            bs = b
-        x += dx; y+= dy;
-
-        if(not self._RangeCheck(x,y)):
+    def _MoveLine(self,x,y,dx,dy,p,e,c=False):
+        b = self.board
+        xx = x + dx; yy = y + dy;
+        if(not self._RangeCheck(xx,yy)):
             return False
-        if(bs[y][x] != e):
+        if(b[yy][xx] != e):
             return False
-        if(not c):
-            bs[y][x] = p 
-        x += dx; y+= dy;
-        if(not self._RangeCheck(x,y)):
+        xx += dx; yy+= dy;
+        if(not self._RangeCheck(xx,yy)):
             return False
-        while(bs[y][x] == e):
-            if(not c):
-                bs[y][x] = p
-            x += dx; y+= dy;
-            if(not self._RangeCheck(x,y)):
+        while(b[yy][xx] == e):
+            xx += dx; yy += dy;
+            if(not self._RangeCheck(xx,yy)):
                 return False
-        if(bs[y][x] == p):
+        if(b[yy][xx] == p):
             if(not c):
-                b[:] = bs[:]
+                while((yy!= y) or (xx != x)):
+                    b[yy][xx] = p
+                    xx-=dx;yy-=dy;
             return True
         return False
     def _RangeCheck(self,x,y):
-        return ((x in range(8)) and (y in range(8)))
+        return (0 <= x) and (x < 8) and (0 <= y) and (y < 8)
             
 
-
-class Move(object):
-    """ Move for Reversi """
-    FIRST  = FIRST
-    SECOND = SECOND
-    def __init__(self, p, x, y, *args, **kwargs):
-        self.player = p
-        self.x = x
-        self.y = y
-        return super().__init__(*args, **kwargs)
-    def __str__(self):
-        if(self.player == Move.FIRST):
-            rv = "ABCDEFGH"[self.x]+"12345678"[self.y]
-        elif(self.player == Move.SECOND):
-            rv = "abcdefgh"[self.x]+"12345678"[self.y]
-        else:
-            rv = "ERROR"
-        return rv
-    def GetX(self):
-        return self.x
-    def GetY(self):
-        return self.y
-    def Me(self):
-        return self.player
-    def Enemy(self):
-        return Enemy(self.player)
 
 class PlayerBase(object):
     """ Base for Player """
@@ -178,7 +176,7 @@ class Game(object):
         if('DebugPrintFinalBoard' in kwargs.keys()):
             board.PrintBoard()
         return board.Score()
-    def ErrorMove(self, player, board, move, **kwargs):
+    def ErrorMove(self, player, board:Board, move:Move, **kwargs):
         if('ErrorPrint'):
             print("Error:")
             board.PrintBoard()
